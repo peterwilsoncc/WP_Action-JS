40fb5761af37eee8565afeb48477da811d1f0c89
Peter Wilson <git-commit-by@peterwilson.cc>
2013-12-07 13:54:53 +1100

    prepare for API

---
+++ wp-action-filter.js
@@ -0,0 +1,48 @@
+(WP = function( window, document ) {
+	var action_id = 1,
+		filter_id = 1;
+	
+	var has_action = function() {
+	}
+
+	var add_action = function(){
+	}
+
+	var do_action = function(){
+	}
+
+	var do_action_ref_array = function(){
+	}
+
+	var did_action = function(){
+	}
+
+	var remove_action = function(){
+	}
+
+	var remove_all_actions = function(){
+	}
+	
+	var has_filter = function() {
+	}
+
+	var add_filter = function(){
+	}
+
+	var apply_filter = function(){
+	}
+
+	var apply_filter_ref_array = function(){
+	}
+
+	var current_filter = function(){
+	}
+
+	var remove_filter = function(){
+	}
+
+	var remove_all_filters = function(){
+	}
+
+}( this, document ));
+
